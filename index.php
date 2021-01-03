961060ad4963aec07b8a17ddefdf221ec59aba0a
shakib77 <shakibshaon77@gmail.com>
2021-01-03 22:50:20 +0600

    added insert table, jquery, edit modal

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 <!--database connection-->
 
 <?php
+
+    $insert = false;
+
     $servername = "localhost";
     $username = "root";
     $password = "";
@@ -20,12 +23,16 @@
         $result = mysqli_query($conn, $sql);
 
         if ($result) {
-            echo "The record has been inserted successfully<br>";
+//            echo "The record has been inserted successfully<br>";
+            $insert = true;
         } else {
             echo "The record was not inserted successfully because of this error --> " . mysqli_error($conn);
         }
     }
 ?>
+
+<!--Database connection end-->
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -36,9 +43,49 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 
+    <link rel="stylesheet" href="//cdn.datatables.net/1.10.22/css/jquery.dataTables.min.css">
+
+    <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
+
+    <script src="//cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
+
+    <script>
+        $(document).ready( function () {
+            $('#myTable').DataTable();
+        } );
+    </script>
+
     <title>PHP CRUD - Project-eNotes</title>
 </head>
 <body>
+
+    <!--    Edit modal-->
+
+    <!-- Button trigger modal -->
+    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editModal">
+        Edit Modal
+    </button>
+
+    <!-- Modal -->
+    <div class="modal fade" id="editModal" tabindex="-1" aria-labelledby="editModal" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="editModalLabel">Modal title</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    ...
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!--    Edit modal ends-->
 
     <!--Nav bar starts-->
 
@@ -70,9 +117,18 @@
 
 <!--    Nav bar ends-->
 
-    <div class="container my-3">
+    <?php
+        if ($insert) {
+            echo "<div class='alert alert-success alert-dismissible fade show' role='alert'>
+                    <strong>Success!</strong> Your note has been recorded successfully.
+                    <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+                </div>";
+        }
+    ?>
+
+    <div class="container my-4">
         <h2>Add a note</h2>
-        <form action="/project/crud/index.php" method="post">
+        <form action="/projects/crud/index.php" method="post">
             <div class="mb-3">
                 <label for="title" class="form-label">Note Title</label>
                 <input type="text" class="form-control" id="title" name="title">
@@ -85,8 +141,8 @@
         </form>
     </div>
 
-    <div class="container">
-        <table class="table">
+    <div class="container" my-4>
+        <table class="table" id="myTable">
             <thead>
             <tr>
                 <th scope="col">S.NO</th>
@@ -100,22 +156,29 @@
             <?php
             $sql = "SELECT * FROM `notes`";
             $result = mysqli_query($conn, $sql);
+            $sno = 0;
 
             while ($row = mysqli_fetch_assoc($result)) {
+                $sno = $sno + 1;
                 echo "<tr>
-                <th scope='row'>". $row['sno'] . "</th>
+                <th scope='row'>". $sno . "</th>
                 <td>". $row['title'] . "</td>
                 <td>". $row['description'] . "</td>
-                <td>Actions</td>
+                <td>
+                    <a href='/edit'>Edit</a>
+                    <a href='/del'>Delete</a>
+                </td>
+                
             </tr>";
             }
             ?>
             </tbody>
         </table>
     </div>
+    <hr>
 
 
-<!-- Bootstrap Bundle with Popper -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+    <!-- Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 </body>
 </html>
